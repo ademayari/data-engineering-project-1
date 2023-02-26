349803237a01d6f4b60b57892ee202ea96830ab4
Adem Ayari <adem.ayari@student.hogent.be>
2023-02-26 01:39:54 +0100

    Enkel nog gescrapede stuff in een csv en datum

--- Ryanair_data/Ryanair_Scrape.py
+++ Ryanair_data/Ryanair_Scrape.py
@@ -15,33 +15,69 @@ ONE_WAY_URL = "https://www.ryanair.com/api/booking/v4/nl-nl/availability?ADT=1&C
 
 
 
-spanje = ["ALC", "IBZ", "AGP", "PMI", "TCI", "TVI"]
+spanje = ["ALC", "IBZ", "AGP", "PMI"]
 italie = ["BDS", "NAP", "PMO"]
 portugal = ["FAO"]
 griekenland = ["HER", "RHO","CFU"]
 luchthaven = ["BRU", "CRL"]
 
-# change desitination code in URL
-# def change_destination_code(destination_code):
-#     global ONE_WAY_URL
-#     ONE_WAY_URL = ONE_WAY_URL.replace("AGP", destination_code)
-    
+list_of_destinations = spanje + italie + portugal + griekenland 
 
 
-page = requests.get(ONE_WAY_URL)
+number = 0
 
-soup = BeautifulSoup(page.content, "lxml")
-print(soup)
-result = soup.find("p").text
-print(type(result))
 
-#convert string to  object
-json_object = json.loads(result)
+# change desitination code in URL 
+for i in list_of_destinations:
+    number += 1
+
+    NEW_ONE_WAY_URL = ONE_WAY_URL.replace("AGP", i)
+    # print new empty line
+    #print("--------------------------------------------------------------")
+    #print(NEW_ONE_WAY_URL, number)
+    page = requests.get(NEW_ONE_WAY_URL)
+    soup = BeautifulSoup(page.content, "lxml")
+    #print(soup)
+    result = soup.find("p").text
+    #print(type(result))
+    #convert string to  object
+    json_object = json.loads(result)
+    #print(json_object)
+    # get columns originName, destinationName, dateOut, amount from the json object by using the json object 
+    print("Destination: ", json_object["trips"][0]["destinationName"])
+    print("Date Out: ",  json_object["trips"][0]["dates"][0]["dateOut"])
+
+    #if there is no flight available, the json object will not have the key "flights"
+    # check if the key "flights" is in the json object and check if the list is not empty
+
+
+    if json_object["trips"][0]["dates"][0]["flights"] != []:
+        print("Flight: ",  json_object["trips"][0]["dates"][0]["flights"][0]["flightNumber"])
+        print("Value: ",  json_object["trips"][0]["dates"][0]["flights"][0]["regularFare"]["fares"][0]["amount"])
+    else:
+        print("No flight available")
+
+    #print("Value: ",  json_object["trips"][0]["dates"][0]["flights"][0]["regularFare"]["fares"][0]["amount"])
+
+
+
+
+# page = requests.get(ONE_WAY_URL)
+
+# soup = BeautifulSoup(page.content, "lxml")
+# print(soup)
+# result = soup.find("p").text
+# print(type(result))
+
+# #convert string to  object
+# json_object = json.loads(result)
 
 #check new data type
 #print(type(json_object))
 
 #output
-#<class 'dict'>
+# get the price of the flight from the soup object as well as destination and date and time 
+# print(json_object["outbound"]["fares"][0]["price"]["value"])
 
 
+# get originName, destinationName, dateOut, amount from the json object 
