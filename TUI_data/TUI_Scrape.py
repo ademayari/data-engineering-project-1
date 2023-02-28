63c3bec9575d635c5b5d9acbae3fc95db7a2e5f9
Luka <Luka.Leroy@student.hogent.be>
2023-02-28 17:54:54 +0100

    Update TUI_Scrape

--- TUI_data/TUI_Scrape.py
+++ TUI_data/TUI_Scrape.py
@@ -1,3 +1,6 @@
+import csv
+import os
+import json
 from selenium import webdriver
 from datetime import datetime
 from selenium.webdriver.chrome.service import Service
@@ -6,12 +9,16 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
-# from webdriver_manager.chrome import ChromeDriverManager 
-import json
 
-PATH = "C:/School/Dataengineeringproject/chromedriver_win32/chromedriver.exe"
+PATH = "C:\School\Data engineering project\chromedriver_win32\chromedriver.exe"
 
-url = "https://www.tuifly.be/flight/nl/search?flyingFrom%5B%5D=BRU&flyingTo%5B%5D=AGP&depDate=2023-04-07&adults=1&children=0&childAge=&choiceSearch=true&searchType=pricegrid&nearByAirports=true&currency=EUR&isOneWay=true&returnDate=2023-04-14"
+destinations = {
+    "spanje": ["ALC", "IBZ", "AGP", "PMI"],
+    "italie": ["BDS", "NAP", "PMO"],
+    "portugal": ["FAO"],
+    "griekenland": ["HER", "RHO","CFU"]
+}
+
 
 
 options = webdriver.ChromeOptions()
@@ -21,29 +28,57 @@ driver_service = Service(executable_path=PATH)
 driver = webdriver.Chrome(service=driver_service,options=options)
 driver.maximize_window()
 driver.implicitly_wait(25)
-driver.get(url)
 
-driver.find_element(By.CSS_SELECTOR, "#cmCloseBanner").click()
+for country, codes in destinations.items():
+    for code in codes:
+        url = f"https://www.tuifly.be/flight/nl/search?flyingFrom%5B%5D=BRU&flyingTo%5B%5D={code}&depDate=2023-04-07&adults=1&children=0&childAge=&choiceSearch=true&searchType=pricegrid&nearByAirports=true&currency=EUR&isOneWay=true&returnDate=2023-04-14"
+        driver.get(url)
+        driver.find_element(By.CSS_SELECTOR, "#cmCloseBanner").click()
 
+        element = WebDriverWait(driver, 50).until(
+            EC.presence_of_element_located((By.CSS_SELECTOR, "div#page div.container footer > script"))
+        )
 
-element = WebDriverWait(driver, 50).until(
-        EC.presence_of_element_located((By.CSS_SELECTOR, "div#page div.container footer > script"))
-    ) 
+# retrieve the data from the var searchResultsJson
+data = driver.execute_script("return searchResultsJson")
 
-# selection = driver.find_element(By.CSS_SELECTOR, "div#page div.container footer script")
-result = element.get_attribute('innerHTML')
-
-print(result)
-
-#print result into json file
-
-
+print(data)
 
 
-#get departureAirportNames departureDate currentPrice from the csv file
+# get the flight search criteria
+search_criteria = data['flightSearchCriteria']
 
 
-#driver.quit()
+dep_airport = data['depAirportData'][0] 
+dep_airport_name = dep_airport['name']
+dep_airport_code = dep_airport['id']
+arr_airport = data['arrAirportData'][0] 
+arr_airport_name = arr_airport['name']
+arr_airport_code = arr_airport['id']
+
+
+outbound_data = data['dateAvailabilityData']['outboundAvailabilityData']
+selected_date_data = next(filter(lambda x: x['selected'], outbound_data))
+price = selected_date_data['currencyAppendedPrice']
+
+
+
+file_path = 'TUI_data/flight_data.csv'
+
+
+if os.path.exists(file_path):
+    
+    with open(file_path, mode='a', newline='') as file:
+        writer = csv.writer(file)
+        
+        writer.writerow([dep_airport_name, dep_airport_code, arr_airport_name, arr_airport_code, price])
+else:
+    
+    with open(file_path, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(['Departure Airport Name', 'Departure Airport Code', 'Arrival Airport Name', 'Arrival Airport Code', 'Price'])
+        
+        writer.writerow([dep_airport_name, dep_airport_code, arr_airport_name, arr_airport_code, price])
 
 
 
