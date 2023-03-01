bde4640ffb4bd454e5760b83b7f7252663bf627c
Adem Ayari <adem.ayari@student.hogent.be>
2023-03-01 22:31:47 +0100

    miauw

--- Ryanair_data/Ryanair_Scrape.py
+++ Ryanair_data/Ryanair_Scrape.py
@@ -21,25 +21,57 @@ list_of_destinations = spanje + italie + portugal + griekenland
 datenow = datetime.now().strftime("%Y-%m-%d")
 
 
+print("#################################### BRUSSEL ####################################")
+
+
 # change desitination code in URL 
 for i in list_of_destinations:
     
 
     NEW_ONE_WAY_URL = ONE_WAY_URL.replace("AGP", i)
-    NEW_ONE_WAY_URL = NEW_ONE_WAY_URL.replace("2023-03-02", datenow)
+    NEW_ONE_WAY_URL = NEW_ONE_WAY_URL.replace("2023-03-02", "2023-03-08")
     page = requests.get(NEW_ONE_WAY_URL)
     soup = BeautifulSoup(page.content, "lxml")
-
     result = soup.find("p").text
 
-    #convert string to  object
+    # convert string to  object
     json_object = json.loads(result)
 
     # get columns originName, destinationName, dateOut, amount from the json object by using the json object 
     print("Destination: ", json_object["trips"][0]["destinationName"])
     print("Date Out: ",  json_object["trips"][0]["dates"][0]["dateOut"])
 
-    #if there is no flight available, the json object will not have the key "flights"
+    # if there is no flight available, the json object will not have the key "flights"
+    # check if the key "flights" is in the json object and check if the list is not empty
+
+    if json_object["trips"][0]["dates"][0]["flights"] != []:
+        print("Flight: ",  json_object["trips"][0]["dates"][0]["flights"][0]["flightNumber"])
+        print("Value: ",  json_object["trips"][0]["dates"][0]["flights"][0]["regularFare"]["fares"][0]["amount"])
+    else:
+        print("No flight available")
+
+
+print("#################################### CHARLEROI ####################################")
+
+# change the destination code in the URL and the origin code in the URL
+for i in list_of_destinations:
+        
+    NEW_ONE_WAY_URL = ONE_WAY_URL.replace("AGP", i)
+    NEW_ONE_WAY_URL = NEW_ONE_WAY_URL.replace("BRU", "CRL")
+    NEW_ONE_WAY_URL = NEW_ONE_WAY_URL.replace("2023-03-02", "2023-03-08")
+    page = requests.get(NEW_ONE_WAY_URL)
+    soup = BeautifulSoup(page.content, "lxml")
+    result = soup.find("p").text
+
+    # convert string to  object
+    json_object = json.loads(result)
+
+    # get columns originName, destinationName, dateOut, amount from the json object by using the json object 
+    print("Origin: ", json_object["trips"][0]["originName"])
+    print("Destination: ", json_object["trips"][0]["destinationName"])
+    print("Date Out: ",  json_object["trips"][0]["dates"][0]["dateOut"])
+
+    # if there is no flight available, the json object will not have the key "flights"
     # check if the key "flights" is in the json object and check if the list is not empty
 
     if json_object["trips"][0]["dates"][0]["flights"] != []:
@@ -50,34 +82,4 @@ for i in list_of_destinations:
 
 
 
-####JUNKYARD####
 
-# page = requests.get(ONE_WAY_URL)
-
-# soup = BeautifulSoup(page.content, "lxml")
-# print(soup)
-# result = soup.find("p").text
-# print(type(result))
-
-# #convert string to  object
-# json_object = json.loads(result)
-
-#check new data type
-#print(type(json_object))
-
-#output
-# get the price of the flight from the soup object as well as destination and date and time 
-# print(json_object["outbound"]["fares"][0]["price"]["value"])
-
-
-# get originName, destinationName, dateOut, amount from the json object 
-
-
-# backup_one_way = "https://www.ryanair.com/api/booking/v4/nl-nl/availability?ADT=1&CHD=0&DateIn=2023-03-14&DateOut=2023-03-02&Destination=AGP&Disc=0&INF=0&Origin=BRU&TEEN=0&promoCode=&IncludeConnectingFlights=false&FlexDaysBeforeOut=2&FlexDaysOut=2&FlexDaysBeforeIn=2&FlexDaysIn=2&RoundTrip=false&ToUs=AGREED"
-
-# alternative_one_way = "https://services-api.ryanair.com/farfnd/3/oneWayFares?&departureAirportIataCode=BCN&language=en&limit=16&market=en-gb&offset=0&outboundDepartureDateFrom=2023-02-28&outboundDepartureDateTo=2023-03-20&priceValueTo=150"
-
-# URL = "https://www.ryanair.com/api/booking/v4/nl-nl/availability?ADT=1&CHD=0&DateIn=2023-03-14&DateOut=2023-03-02&Destination=AGP&Disc=0&INF=0&Origin=BRU&TEEN=0&promoCode=&IncludeConnectingFlights=false&FlexDaysBeforeOut=2&FlexDaysOut=2&FlexDaysBeforeIn=2&FlexDaysIn=2&RoundTrip=true&ToUs=AGREED"
-
-
-####JUNKYARD####
