a8d68567caeb41f4234933e5062a3020fbba455c
Michiel DW <michiel.dewilde@hotmail.com>
2023-03-02 11:56:48 +0100

    fix element locating

--- Transavia_data/Transavia_scrape.py
+++ Transavia_data/Transavia_scrape.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 import os
+import time
 from datetime import datetime
 
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
@@ -30,26 +31,32 @@ TEMP_DATE = "30-05-2023"
 # deselect return flight
 
 def set_departure(driver: webdriver.Chrome, departure = "BRU"):
-    hidden_departure_input = driver.find_element(By.NAME, "routeSelection.DepartureStation")
+    # hidden_departure_input = driver.find_element(By.NAME, "routeSelection.DepartureStation")
+    hidden_departure_input = driver.find_element(By.XPATH, '//*[@name="routeSelection.DepartureStation"]')
     hidden_departure_input.send_keys(departure)
 
 def set_destination(driver: webdriver.Chrome, destination):
-    hidden_destination_input = driver.find_element(By.NAME, "routeSelection.ArrivalStation")
+    # hidden_destination_input = driver.find_element(By.NAME, "routeSelection.ArrivalStation")
+    hidden_destination_input = driver.find_element(By.XPATH, '//*[@name="routeSelection.ArrivalStation"]')
     hidden_destination_input.send_keys(destination)
     
 def set_one_way(driver: webdriver.Chrome):
-    is_return_flight_input = driver.find_element(By.ID, "dateSelection.isReturnFlight")
+    # is_return_flight_input = driver.find_element(By.ID, "dateSelection.isReturnFlight")
+    is_return_flight_input = driver.find_element(By.XPATH, '//*[@id="dateSelection.isReturnFlight"]')
     if is_return_flight_input.is_selected(): is_return_flight_input.click()
 
 def set_date(driver: webdriver.Chrome, date):
-    date_input = driver.find_element(By.ID, "dateSelection_OutboundDate-datepicker")
+    # date_input = driver.find_element(By.ID, "dateSelection_OutboundDate-datepicker")
+    date_input = driver.find_element(By.XPATH, '//*[@id="dateSelection_OutboundDate-datepicker"]')
     date_input.send_keys(date)
 
 def submit_form(driver: webdriver.Chrome):
-    form = driver.find_element(By.ID, "desktop")
+    # form = driver.find_element(By.ID, "desktop")
+    form = driver.find_element(By.XPATH, '//*[@id="desktop"]')
     form.submit()
 
 def scrape_price(driver: webdriver.Chrome, to, date = TEMP_DATE):
+    time.sleep(30)
     set_departure(driver)
     set_destination(driver, to)
     set_one_way(driver)
