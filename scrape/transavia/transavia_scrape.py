3165397a913356773dc794682761a52624d426d7
Adem Ayari <adem.ayari@student.hogent.be>
2023-03-19 00:05:41 +0100

    transavia cleanup + merge fix

--- scrape/transavia/transavia_scrape.py
+++ scrape/transavia/transavia_scrape.py
@@ -13,6 +13,10 @@ from selenium.webdriver.support.wait import WebDriverWait as wait
 from gmail import login_gmail
 from captcha import solve_captcha_if_present
 import itertools
+from gmail import *
+from captcha import *
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
 
 URL = "https://www.transavia.com/nl-BE/home/"
 
@@ -33,40 +37,44 @@ DIMENSIONS = [
 
 TEMP_DATE = "30-05-2023"
 
-# FROM: Brussels
-# TO: x
-# date: today + (4 hours, 1 day, 7 days, 14 days, 1 month, 2 months, 4 months, 6 months)
-# people: 1 adult
-# deselect return flight
-
-def set_departure(driver: webdriver.Chrome, departure = "BRU"):
+def set_departure(driver: webdriver.Chrome, departure = "Brussel, België"):
     # hidden_departure_input = driver.find_element(By.NAME, "routeSelection.DepartureStation")
-    hidden_departure_input = driver.find_element(By.XPATH, '//*[@name="routeSelection.DepartureStation"]')
-    hidden_departure_input.send_keys(departure)
+    hidden_departure_input = driver.find_element(By.XPATH, '//*[@id="routeSelection_DepartureStation-input"]')
+    # click the departure input
+    hidden_departure_input.click()
+    # select the first element from the dropdown
+    first_element = driver.find_element(By.XPATH, '/html/body/div[5]/div/div[1]/section/div/section/div[1]/div[2]/section/form/section/div[2]/div[1]/div[1]/div/div/div[1]/section/ul/li[1]')
+    first_element.click()
+#    hidden_departure_input.send_keys(departure)
+    time.sleep(10)
 
-def set_destination(driver: webdriver.Chrome, destination):
+def set_destination(driver: webdriver.Chrome, destination= "Malaga, Spanje"):
     # hidden_destination_input = driver.find_element(By.NAME, "routeSelection.ArrivalStation")
-    hidden_destination_input = driver.find_element(By.XPATH, '//*[@name="routeSelection.ArrivalStation"]')
+    hidden_destination_input = driver.find_element(By.XPATH, '//*[@id="routeSelection_ArrivalStation-input"]')
     hidden_destination_input.send_keys(destination)
+    time.sleep(10)
     
 def set_one_way(driver: webdriver.Chrome):
     # is_return_flight_input = driver.find_element(By.ID, "dateSelection.isReturnFlight")
     is_return_flight_input = driver.find_element(By.XPATH, '//*[@id="dateSelection.isReturnFlight"]')
     if is_return_flight_input.is_selected(): is_return_flight_input.click()
+    time.sleep(10)
 
 def set_date(driver: webdriver.Chrome, date):
     # date_input = driver.find_element(By.ID, "dateSelection_OutboundDate-datepicker")
     date_input = driver.find_element(By.XPATH, '//*[@id="dateSelection_OutboundDate-datepicker"]')
     date_input.send_keys(date)
+    time.sleep(10)
 
 def submit_form(driver: webdriver.Chrome):
     # form = driver.find_element(By.ID, "desktop")
     form = driver.find_element(By.XPATH, '//*[@id="desktop"]')
     form.submit()
+    time.sleep(10)
 
 def init_transavia_search(driver: webdriver.Chrome):
     driver.get(URL)
-    time.sleep(3)
+    time.sleep(10)
 
     captcha_result = solve_captcha_if_present(driver)
     if captcha_result:
@@ -78,14 +86,21 @@ def randomise_res(driver: webdriver.Chrome):
     driver.set_window_size(size[0], size[1])
 
 def scrape_price(driver: webdriver.Chrome, to, date = TEMP_DATE):
-    time.sleep(10000)
+    time.sleep(5000)
     set_departure(driver)
     set_destination(driver, to)
     set_one_way(driver)
     set_date(driver, date)
     submit_form(driver)
+    time.sleep(30)
 
+def uitgebreid_zoeken(driver: webdriver.Chrome):
+    zoeken = driver.find_element(By.CSS_SELECTOR, 'li.HV-gs-type-e--bp0:nth-child(1) > a:nth-child(1)')
+    zoeken.click()
 
+def uitgebreid_zoeken(driver: webdriver.Chrome):
+    zoeken = driver.find_element(By.CSS_SELECTOR, 'li.HV-gs-type-e--bp0:nth-child(1) > a:nth-child(1)')
+    zoeken.click()
 
 def main(driver):
     try:
@@ -106,6 +121,25 @@ def main(driver):
 
     
     # scrape_price(driver, 'IBZ')
+
+# def main():
+    
+#     login_gmail(driver=driver)
+
+#     time.sleep(30)
+
+#     driver.get(URL)
+#     time.sleep(20)
+    
+
+#     click_captcha(driver)
+
+#     time.sleep(20)
+#     uitgebreid_zoeken(driver)
+#     time.sleep(30)
+
+
+#     scrape_price(driver, 'IBZ')
 
 
 if __name__ == "__main__":
