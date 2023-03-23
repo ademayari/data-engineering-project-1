2ebc80877350a61134fce4c3a3c04ea917f11358
Michiel DW <michiel.dewilde@hotmail.com>
2023-03-23 11:28:29 +0100

    transavia temp results + requests in order

--- scrape/transavia/temp.py
+++ scrape/transavia/temp.py
@@ -1,57 +1,64 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
+from requests.exceptions import JSONDecodeError
 
-import time
+from api import request_flights
+from config import DEPARTURE
 
+def temp_results():
+    print("Tijdelijke resultaten:")
 
-def set_departure(driver: webdriver.Chrome, departure = "Brussel, België"):
-    # hidden_departure_input = driver.find_element(By.NAME, "routeSelection.DepartureStation")
-    hidden_departure_input = driver.find_element(By.XPATH, '//*[@id="routeSelection_DepartureStation-input"]')
-    # click the departure input
-    hidden_departure_input.click()
-    # select the first element from the dropdown
-    first_element = driver.find_element(By.XPATH, '/html/body/div[5]/div/div[1]/section/div/section/div[1]/div[2]/section/form/section/div[2]/div[1]/div[1]/div/div/div[1]/section/ul/li[1]')
-    first_element.click()
-#    hidden_departure_input.send_keys(departure)
-    time.sleep(10)
+    # BRU -> ALC 15/05 -> 20/05
+    count = 0
+    for date in range(20230515, 20230521):
+        try:
+            res = request_flights(DEPARTURE, "ALC", str(date))
+            j = res.json()
+            count += int(j["resultSet"]["count"])
+        except JSONDecodeError:
+            continue
+    print(f"Totaal aantal transavia (BRU->ALC) vluchten van 15/05/2023 tot en met 20/05/2023: {count}")
 
-def set_destination(driver: webdriver.Chrome, destination= "Malaga, Spanje"):
-    # hidden_destination_input = driver.find_element(By.NAME, "routeSelection.ArrivalStation")
-    hidden_destination_input = driver.find_element(By.XPATH, '//*[@id="routeSelection_ArrivalStation-input"]')
-    hidden_destination_input.send_keys(destination)
-    time.sleep(10)
+    # BRU -> FAO average price
+    prices = []
+    for date in range(20230601, 20230631):
+        try:
+            res = request_flights(DEPARTURE, "FAO", str(date))
+            j = res.json()
+            l = [flight["pricingInfoSum"]["totalPriceOnePassenger"] for flight in j["flightOffer"]]
+            prices.extend(l)
+        except JSONDecodeError:
+            continue
+    avg = sum(prices)/len(prices)
+    print(f"Gemiddelde prijs voor alle transavia vluchten naar Faro in de maand juni: €{avg}")
+
+    # BRU -> IBZ departure time 24/05
+    try:
+        res = request_flights(DEPARTURE, "IBZ", "20230524")
+        j = res.json()
+        time = j["flightOffer"][0]["outboundFlight"]["departureDateTime"]
+        time = time[time.index("T")+1:]
+        print(f"Vertrektijd van BRU -> IBZ op 24 mei: {time}")
+    except JSONDecodeError:
+        print("no flight found 24/05")
     
-def set_one_way(driver: webdriver.Chrome):
-    # is_return_flight_input = driver.find_element(By.ID, "dateSelection.isReturnFlight")
-    is_return_flight_input = driver.find_element(By.XPATH, '//*[@id="dateSelection.isReturnFlight"]')
-    if is_return_flight_input.is_selected(): is_return_flight_input.click()
-    time.sleep(10)
+    # BRU -> TFS arrival time 29/07
+    try:
+        res = request_flights(DEPARTURE, "TFS", "20230729")
+        j = res.json()
+        time = j["flightOffer"][0]["outboundFlight"]["arrivalDateTime"]
+        time = time[time.index("T")+1:]
+        print(f"Aankomsttijd van BRU -> TFS op 29 juli: {time}")
+    except JSONDecodeError:
+        print("no flight found 29/07")
 
-def set_date(driver: webdriver.Chrome, date):
-    # date_input = driver.find_element(By.ID, "dateSelection_OutboundDate-datepicker")
-    date_input = driver.find_element(By.XPATH, '//*[@id="dateSelection_OutboundDate-datepicker"]')
-    date_input.send_keys(date)
-    time.sleep(10)
-
-def submit_form(driver: webdriver.Chrome):
-    # form = driver.find_element(By.ID, "desktop")
-    form = driver.find_element(By.XPATH, '//*[@id="desktop"]')
-    form.submit()
-    time.sleep(10)
-
-
-
-
-
-def scrape_price(driver: webdriver.Chrome, to, date = TEMP_DATE):
-    time.sleep(5000)
-    set_departure(driver)
-    set_destination(driver, to)
-    set_one_way(driver)
-    set_date(driver, date)
-    submit_form(driver)
-    time.sleep(30)
-
-def uitgebreid_zoeken(driver: webdriver.Chrome):
-    zoeken = driver.find_element(By.CSS_SELECTOR, 'li.HV-gs-type-e--bp0:nth-child(1) > a:nth-child(1)')
-    zoeken.click()
+    # BRU -> HER
+    count = 0
+    dates = list(range(20230701,20230732))
+    dates.extend(range(20230801, 20230816))
+    for date in dates:
+        try:
+            res = request_flights(DEPARTURE, "HER", str(date))
+            j = res.json()
+            count += int(j["resultSet"]["count"])
+        except JSONDecodeError:
+            continue
+    print(f"Totaal aantal transavia (BRU->HER)vluchten van 01/07/2023 tot en met 15/08/2023: {count}")
