dfecd1d83504688e67c98c3d0479cf790e0f64f1
Adem Ayari <adem.ayari@student.hogent.be>
2023-03-28 20:17:33 +0200

    scrape.py small change import

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ import argparse
 from datetime import datetime
 from date_generator import generate_files, read_files
 from scrape.brussels_airlines.brussels_airlines_scrape import *
-from scrape.ryanair.ryanair import *
+from scrape.ryanair.ryanair import ryanair_scrape
 # from scrape.tui.tui_scrape import *
 
 # Generate files containing dates and destinations
