bfacf8d2543741c55a4c89be15184763c17bf9b6
Adem Ayari <adem.ayari@student.hogent.be>
2023-03-29 13:55:53 +0200

    updated ryanair with error handling

--- scrape/ryanair/ryanair.py
+++ scrape/ryanair/ryanair.py
@@ -20,42 +20,77 @@ list_of_destinations = spanje + italie + portugal + griekenland
 
 # huidige datum
 datenow = datetime.now().strftime("%Y-%m-%d")
-date = "2023-03-27"
+#date = "2023-03-27"
 
 
 
 def ryanair_scrape(date):
-    for i in list_of_destinations:
-        for j in luchthaven:
-            
-            NEW_ONE_WAY_URL = ONE_WAY_URL.replace("AGP", i)
-            NEW_ONE_WAY_URL = NEW_ONE_WAY_URL.replace("2023-03-02", date)
-            page = requests.get(NEW_ONE_WAY_URL)
-            soup = BeautifulSoup(page.content, "lxml")
-            result = soup.find("p").text
-            json_object = json.loads(result)
-            aantal_vluchten = len(json_object["trips"][0]["dates"][0]["flights"])
-            
-            # get columns originName, destinationName, dateOut, value, flightDuration, faresLeft and flightKey from the json object and add them to ryanair.csv
-            if json_object["trips"][0]["dates"][0]["flights"] != []:
+    for destination, dates in date.items():
+        for i in dates:
+            for j in luchthaven:
+                
+                NEW_ONE_WAY_URL = ONE_WAY_URL.replace("AGP", destination)
+                NEW_ONE_WAY_URL = NEW_ONE_WAY_URL.replace("03-02", str(i))
+                for l in range(3):
+                    try:
+                        page = requests.get(NEW_ONE_WAY_URL)
+                        
+                        soup = BeautifulSoup(page.content, "lxml")
+                        result = soup.find("p").text
+                        json_object = json.loads(result)
+                        break
+                    except requests.exceptions.RequestException as e:
+                        print(f"Error connecting to {NEW_ONE_WAY_URL}: {e}")
 
-                for j in range(aantal_vluchten):
-                    originName = json_object["trips"][0]["originName"]
-                    destinationName = json_object["trips"][0]["destinationName"]
-                    dateOut = json_object["trips"][0]["dates"][0]["dateOut"]
-                    prijs = json_object["trips"][0]["dates"][0]["flights"][j]["regularFare"]["fares"][0]["amount"]
-                    timeOut = json_object["trips"][0]["dates"][0]["flights"][j]["segments"][0]["time"][0]
-                    timeArrival = json_object["trips"][0]["dates"][0]["flights"][j]["segments"][0]["time"][1]
-                    flightDuration = json_object["trips"][0]["dates"][0]["flights"][j]["segments"][0]["duration"]
-                    plaatsenOver = json_object["trips"][0]["dates"][0]["flights"][j]["faresLeft"]
-                    flightKey = json_object["trips"][0]["dates"][0]["flights"][j]["flightKey"]
-                    flightNumber = json_object['trips'][0]['dates'][0]['flights'][j]['flightNumber']
+                        if l == 2:
+                            print(f"Could not connect to {NEW_ONE_WAY_URL} after 3 attempts. Skipping.") 
+                try:
+                    aantal_vluchten = len(json_object["trips"][0]["dates"][0]["flights"])
+                except KeyError:
+                    aantal_vluchten = 0
+                
+                # get columns originName, destinationName, dateOut, value, flightDuration, faresLeft and flightKey from the json object and add them to ryanair.csv
+                if aantal_vluchten != 0:
+                    if json_object["trips"][0]["dates"][0]["flights"] != []:
+
+                        for j in range(aantal_vluchten):
+                            originName = json_object["trips"][0]["originName"]
+                            destinationName = json_object["trips"][0]["destinationName"]
+                            dateOut = json_object["trips"][0]["dates"][0]["dateOut"]
+                            try:
+                                prijs = json_object["trips"][0]["dates"][0]["flights"][j]["regularFare"]["fares"][0]["amount"]
+                            except KeyError:
+                                prijs = "None"
+                            try:
+                                timeOut = json_object["trips"][0]["dates"][0]["flights"][j]["segments"][0]["time"][0]
+                            except KeyError:
+                                timeOut = "None"
+                            try:
+                                timeArrival = json_object["trips"][0]["dates"][0]["flights"][j]["segments"][0]["time"][1]
+                            except KeyError:
+                                timeArrival = "None"
+                            try:    
+                                flightDuration = json_object["trips"][0]["dates"][0]["flights"][j]["segments"][0]["duration"]
+                            except KeyError:
+                                flightDuration = "None"
+                            try:    
+                                plaatsenOver = json_object["trips"][0]["dates"][0]["flights"][j]["faresLeft"]
+                            except KeyError:
+                                plaatsenOver = "None"
+                            try:    
+                                flightKey = json_object["trips"][0]["dates"][0]["flights"][j]["flightKey"]
+                            except KeyError:
+                                flightKey = "None"
+                            try:
+                                flightNumber = json_object['trips'][0]['dates'][0]['flights'][j]['flightNumber']
+                            except KeyError:
+                                flightNumber = "None"
 
 
-                    with open('ryanair.csv', "a", newline='') as csv_file:
-                        csv_writer = csv.writer(csv_file)
-                        csv_writer.writerow([])
-                        csv_writer.writerow([originName, destinationName, dateOut, prijs, flightDuration, plaatsenOver, flightKey, flightNumber, timeOut, timeArrival])      
+                            with open('ryanair.csv', "a", newline='') as csv_file:
+                                csv_writer = csv.writer(csv_file)
+                                csv_writer.writerow([])
+                                csv_writer.writerow([originName, destinationName, dateOut, prijs, flightDuration, plaatsenOver, flightKey, flightNumber, timeOut, timeArrival])      
 
 
-ryanair_scrape(date)
+#ryanair_scrape(date)
