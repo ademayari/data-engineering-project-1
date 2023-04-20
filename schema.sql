1deaa6396b835cc3782d09f8b0dc841883f5e725
Michiel DW <michiel.dewilde@hotmail.com>
2023-04-20 14:27:39 +0200

    fixing schema, adding files from chamilo

--- schema.sql
+++ schema.sql
@@ -1,28 +1,31 @@
 ﻿create table airline
 (
-    code    varchar(20) not null
+    code    varchar(50) not null
         primary key,
-    name    varchar(50) null,
-    country varchar(20) null
+    airline_name    varchar(50) not null,
+    country varchar(50) not null
 );
 
 create table airport
 (
-    code    varchar(20) not null
+    code    varchar(50) not null
         primary key,
-    name    varchar(50) null,
-    country varchar(20) null
+    airport_name    varchar(50) not null,
+    country varchar(20) not null
 );
 
 create table flight
 (
-    flight_id      varchar(20)               not null
+    flight_id      varchar(100)               not null
         primary key,
-    airline_code   varchar(20)               null,
-    departure      varchar(20) default 'BRU' null,
-    destination    varchar(20)               null,
-    departure_time date                      null,
-    arrival_time   date                      null,
+    airline_code   varchar(50)               not null,
+    departure      varchar(20) default 'BRU' not null,
+    destination    varchar(20)               not null,
+    departure_date date                     not null,
+    departure_time time                      not null,
+    arrival_date    date                    not null,
+    arrival_time   time                      not null,
+    duration        time                    not null,
     stops          int                       null,
     constraint flight_ibfk_1
         foreign key (airline_code) references airline (code),
@@ -43,11 +46,12 @@ create index destination
 
 create table flight_data
 (
-    flight_id    varchar(20) not null,
+    flight_data_id  int auto_increment not null unique,
+    flight_id    varchar(100) not null,
     time_of_data date        not null,
-    price        float       null,
+    price        float       not null,
     seats_left   int         null,
-    primary key (flight_id, time_of_data),
+    primary key (flight_data_id),
     constraint flight_data_ibfk_1
         foreign key (flight_id) references flight (flight_id)
 );
