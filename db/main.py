2a7cc66f6e1473b2f2d01ace7b3d8ccd5fcdf94a
Adem Ayari <adem.ayari@student.hogent.be>
2023-05-09 20:36:32 +0200

    vic data dump

--- db/main.py
+++ db/main.py
@@ -1,5 +1,22 @@
-from mysql.connector import (connection)
+from mysql.connector import (connection, errorcode, Error as Mysql_error)
 
-cnx = connection.MySQLConnection(user="root", password="VicDep12", host="vichogent.be:40068", database="database1")
+try:
+    cnx = connection.MySQLConnection(user="root", password="VicDep12", host="localhost", database="databank1")
+    cursor = cnx.cursor()
 
-cnx.close()
+
+    query = ("SELECT * FROM airline ")
+    cursor.execute(query)
+
+    for (code, name, country) in cursor:
+        print(f"{code} {name} {country}")
+
+except Mysql_error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Something is wrong with your user name or password")
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print("Database does not exist")
+    else:
+        print(err)
+else:
+  cnx.close()
